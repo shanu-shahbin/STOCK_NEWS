521522760885a687bd2543edeac4501ab68ca935
shanu shahbin <shanushahbin11@gmail.com>
2023-09-21 10:14:27 +0530

    stock api and twilio number sid added

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from twilio.rest import client
+
+VIRTUAL_TWILIO_NUMBER = "+12565884698"
+VERIFIED_NUMBER = "9746558679"
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "BEV1FE89C8NG0E2Y"
+NEWS_API_KEY = "YOUR OWN API KEY FROM NEWSAPI"
+TWILIO_SID = "AC4c7fc74c5bc8a448e74dabc2fed7aea3"
+TWILIO_AUTH_TOKEN = "31200bc81fa562c7d8cca8a1fa7b4547"
