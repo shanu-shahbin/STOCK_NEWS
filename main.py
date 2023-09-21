ee29ea05afd45b864d6fd9270ceaaa368504cfb1
shanu shahbin <shanushahbin11@gmail.com>
2023-09-21 11:27:35 +0530

    final code

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "BEV1FE89C8NG0E2Y"
-NEWS_API_KEY = "YOUR OWN API KEY FROM NEWSAPI"
+NEWS_API_KEY = "d3f30e21cd064c528c3022721ce5f034"
 TWILIO_SID = "AC4c7fc74c5bc8a448e74dabc2fed7aea3"
 TWILIO_AUTH_TOKEN = "31200bc81fa562c7d8cca8a1fa7b4547"
 
@@ -40,3 +40,30 @@ if difference > 0:
 else:
     up_down = "🔻"
 
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+print(diff_percent)
+
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    three_articles = articles[:3]
+    print(three_articles)
+
+    formatted_articles = [
+        f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for
+        article in three_articles]
+    print(formatted_articles)
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
